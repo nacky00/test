7f95d4b2a663584c95bc33e591f1c2834123077e
nacky00 <nacky00eule@gmail.com>
2021-07-28 22:59:03 +0900

    add css constants

--- corporate-site/src/components/organisms/Header/index.js
+++ corporate-site/src/components/organisms/Header/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'components/Router'
 
 class Header extends React.Component {
     render() {
         return (
-            <header style = {{ height: 60, background: "#ddd", padding: 24 }} >
-                original header
+            <header style={headerStyle.header}>
+                <div>LOGO</div>
+                <div>
+                    <Link to="/blog/">About</Link>
+                    <Link to="/blog/">Product</Link>
+                    <Link to="/blog/">News</Link>
+                    <Link to="/blog/">Members</Link>
+                    <Link to="/blog/">Company</Link>
+                    <Link to="/blog/">Contact</Link>
+                </div>
             </header>
         );
     }
 }
 
-export default Header;
+const headerStyle = {
+        header: {
+            background: '#000000',
+            padding: 24,
+            height: 60
+        }
+}
+
+export default Header
+
