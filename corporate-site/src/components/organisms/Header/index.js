520cf7f54fd884bfc939673c6bc992c409fabba6
nacky00 <nacky00eule@gmail.com>
2021-07-28 23:55:50 +0900

    install styled components

--- corporate-site/src/components/organisms/Header/index.js
+++ corporate-site/src/components/organisms/Header/index.js
@@ -1,31 +1,52 @@
 import React from 'react'
 import { Link } from 'components/Router'
 
+import styled from 'styled-components'
+
+const Nav = styled.div`
+    background-color: #000000;
+    padding: 1.6rem;
+`;
+
+const NavHeader = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const NavLeft = styled.div`
+    color: white;
+`
+
+const NavRight = styled.div`
+    color: white;
+    display: flex;
+`
+
+const Title = styled.h1`
+    font-size: 1.2rem;
+    margin-right: 2rem;
+`
+
+
 class Header extends React.Component {
     render() {
-        return (
-            <header style={headerStyle.header}>
-                <div>LOGO</div>
-                <div>
-                    <Link to="/blog/">About</Link>
-                    <Link to="/blog/">Product</Link>
-                    <Link to="/blog/">News</Link>
-                    <Link to="/blog/">Members</Link>
-                    <Link to="/blog/">Company</Link>
-                    <Link to="/blog/">Contact</Link>
-                </div>
-            </header>
-        );
+            return(
+                <Nav>
+                    <NavHeader>
+                        <NavLeft>RelyonTrip</NavLeft>
+                        <NavRight>
+                            <Link to="/"><Title>About</Title></Link>
+                            <Link to="/"><Title>Products</Title></Link>
+                            <Link to="/"><Title>News</Title></Link>
+                            <Link to="/"><Title>Members</Title></Link>
+                            <Link to="/"><Title>Company</Title></Link>
+                            <Link to="/"><Title>Contact</Title></Link>
+                        </NavRight>
+                    </NavHeader>
+                </Nav>
+            );
     }
 }
 
-const headerStyle = {
-        header: {
-            background: '#000000',
-            padding: 24,
-            height: 60
-        }
-}
-
 export default Header
-
