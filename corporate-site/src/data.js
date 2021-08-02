9e918298b2071c34b2cba1f1d8f86336ccfec34c
nacky00 <nacky00eule@gmail.com>
2021-08-02 17:18:03 +0900

    add news source

---
+++ corporate-site/src/data.js
@@ -0,0 +1,14 @@
+export const newsData = [
+    {
+        date: "2021/08/02",
+        title: "タイトルA",
+        category: "プレスリリース",
+        url: "https://prtimes.jp/main/html/rd/p/000000017.000051474.html"
+    },
+    {
+        date: "2021/08/06",
+        title: "タイトルB",
+        category: "トピック",
+        url: "https://prtimes.jp/main/html/rd/p/000000017.000051474.html"
+    }
+]
