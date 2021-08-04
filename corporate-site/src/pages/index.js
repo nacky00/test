37cb4e195de55b2080852b7d37d3e4ea65589ff2
nacky00 <nacky00eule@gmail.com>
2021-08-04 18:51:25 +0900

    update css

--- corporate-site/src/pages/index.js
+++ corporate-site/src/pages/index.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+import Theme from '../theme'
+
+const MainVisual = styled.div`
+    position: fixed;
+    width: 100%;
+    z-index: -1;
+    top: 0;
+    height: 540px;
+`;
+
+const MainTitle = styled.div`
+  position: "absolute;
+  top: 50%;
+  left: 0;
+  padding-left: 32px
+  z-index: 2;
+  h1 {
+    font-size: ${Theme.font.xlarge};
+    color: ${Theme.color.white};
+  }
+`;
 
 export default () => (
   <div>
-    <div className="main-visual" style={{ position: "fixed", width: "100%", zIndex: -1, top: 0, backgroundColor: "green", height: "540px" }}>
+    <MainVisual>
       <div style={{ position: "absolute", top: "50%", left: 0, paddingLeft: "32px", zIndex: 2 }}>
         <h1 style={{ fontSize: "56px" }}>Something Message</h1>
         <p>message message message</p>
@@ -10,7 +32,8 @@ export default () => (
       <video autoPlay="true" loop="true" muted="true" playsInline width="100%" height="auto" style={{ position: "absolute", top: 0, left: 0, zIndex: 1, display: "block" }}>
         <source src="/sample_movie.mp4" type="video/mp4"/>
       </video>
-    </div>
+    </MainVisual>
+    
     <div className="contents" style={{ backgroundColor: "beige", marginTop: "32%" }}>
         <section>
           <div style={{ backgroundColor: "pink" }}>
