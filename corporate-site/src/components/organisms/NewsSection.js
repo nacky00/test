a2468bc19710b945bcec9c0f413dd6e2fe9b9087
nacky00 <nacky00eule@gmail.com>
2021-08-06 11:53:56 +0900

    update news item style

--- corporate-site/src/components/organisms/NewsSection.js
+++ corporate-site/src/components/organisms/NewsSection.js
@@ -7,13 +7,13 @@ export const NewsSection = props => {
     return (
         <Section>
             <Wrapper>
-                <h1 style={{ color: "white" }}>News</h1>
-                <NewsItem>
-                    <a href=""><p>2021.05.12</p><p>「Okinawa Startup Program」のデモデイが開催され、代表の西村が発表いたしました</p></a>
-                </NewsItem>
-                <NewsItem>
-                    <a href=""><p>2021.05.12</p><p>「Okinawa Startup Program」のデモデイが開催され、代表の西村が発表いたしました</p></a>
-                </NewsItem>
+            <h1 style={{ color: "white" }}>News</h1>
+                <ItemWrapper>
+                    <Item href="" target="_blank">
+                        <Date>2021.05.12</Date>
+                        <Summary>「Okinawa Startup Program」のデモデイが開催され、代表の西村が発表いたしましたあああああああああああああああ</Summary>
+                    </Item>
+                </ItemWrapper>
                 <MoreButton>
                     <Link to="/news">
                         <p>News</p><p>→</p>
@@ -23,14 +23,6 @@ export const NewsSection = props => {
         </Section>
     )
 }
-
-const NewsStoreButton = styled.a.attrs(props => ({
-    width: props.width || "153px",
-}))`
-    display: block;
-    width: ${props => props.width};
-    height: auto;
-`
 
 const Wrapper = styled.div`
     display: flex;
@@ -56,13 +48,25 @@ a {
     }
 }`
 
-const NewsItem = styled.div`
-border-bottom: solid 1px ${Theme.color.white};
-a {
+
+const ItemWrapper = styled.div`
+    padding: 2rem 0;
+    border-bottom: solid 1px ${Theme.color.grey900};
+`
+
+const Item = styled.a`
     display: flex;
     flex-direction: row;
-    align-items: center;
-    p {
-        color: ${Theme.color.white};
-    }
-}`
+`
+
+const Date = styled.div`
+    color: ${Theme.color.grey700};
+    font-weight: ${Theme.weight.normal};
+    letter-spacing: ${Theme.letterSpacing.mediumWide};
+    padding-right: 2rem;
+`
+const Summary = styled.div`
+    color: ${Theme.color.grey400};
+    font-weight: ${Theme.weight.normal};
+    letter-spacing: ${Theme.letterSpacing.mediumWide};
+`
