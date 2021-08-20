3ea1467a12fee4f7d4c34052dafbd326db0dc277
nacky00 <nacky00eule@gmail.com>
2021-08-20 16:26:17 +0900

    create new privacy policy page

---
+++ corporate-site/src/pages/privacy-policy.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+import Theme from 'theme'
+import { NormalWrapperStyle } from 'components/atoms/Wrapper/style'
+
+export default function PricavyPolicy() {
+    return (
+        <div></div>
+        )
+}
