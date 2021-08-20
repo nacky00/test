2d8fe4157396b3de9756386f9faffe0d0c4db97c
nacky00 <nacky00eule@gmail.com>
2021-08-20 17:09:31 +0900

    create container style

---
+++ corporate-site/src/components/atoms/Container/style.js
@@ -0,0 +1,11 @@
+import { css } from "styled-components";
+
+const NormalContainer = css`
+    padding: 2rem 0;
+`;
+
+const SubContainer = css`
+    padding: 1rem 0;
+`;
+
+export { NormalContainer, SubContainer };
--- corporate-site/src/pages/privacy-policy.js
+++ corporate-site/src/pages/privacy-policy.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
-import Theme from 'theme'
 import { NormalWrapperStyle } from 'components/atoms/Wrapper/style'
 import { FullPageSectionStyle  } from 'components/atoms/Section/style'
 import { PageMainTitle, ElementHeader, NormalText, NormalLi, NormalOl } from 'components/atoms/Text/style'
+import { NormalContainer, SubContainer } from 'components/atoms/Container/style'
 
 export default function PricavyPolicy() {
     return (
         <Section>
             <Wrapper>
-
+                <Title>プライバシーポリシー</Title>
             </Wrapper>
         </Section>
         )
@@ -21,6 +21,14 @@ const Section = styled.section`
 
 const Wrapper = styled.div`
     ${NormalWrapperStyle};
+`
+
+const FirstContainer = styled.div`
+    ${NormalContainer};
+`
+
+const SecondContainer = styled.div`
+    ${SubContainer};
 `
 
 const Title = styled.div`
