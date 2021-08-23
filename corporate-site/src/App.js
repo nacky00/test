2dab62f3006f036cd803c57967b8d7740aafe136
nacky00 <nacky00eule@gmail.com>
2021-08-23 19:52:10 +0900

    add scroll top when page is changed

--- corporate-site/src/App.js
+++ corporate-site/src/App.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { Root, Routes, addPrefetchExcludes } from 'react-static'
-//
-import { Link, Router } from 'components/Router'
+import { Link, Router, useLocation } from 'components/Router'
 import Dynamic from 'containers/Dynamic'
 import Footer from 'components/organisms/Footer'
 import Header from 'components/organisms/Header'
@@ -11,12 +10,15 @@ import './app.css'
 // Any routes that start with 'dynamic' will be treated as non-static routes
 addPrefetchExcludes(['dynamic'])
 
+
+
 function App() {
   return (
     <Root>
       <Header />
       <div>
         <React.Suspense fallback={<em>Loading...</em>}>
+        <ScrollToTop />
           <Router>
             <Dynamic path="dynamic" />
             <Routes path="*" />
@@ -26,6 +28,15 @@ function App() {
       <Footer />
     </Root>
   )
+}
+
+// ページ遷移時に上部までスクロール
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useLayoutEffect(() => {
+      window.scrollTo(0, 0)
+  }, [pathname])
+  return null
 }
 
 export default App
