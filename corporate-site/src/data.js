38494ec4d402126ff08d00d907061fe7dce81e9b
nacky00 <nacky00eule@gmail.com>
2021-08-24 13:43:25 +0900

    add news data

--- corporate-site/src/data.js
+++ corporate-site/src/data.js
@@ -1,44 +1,177 @@
 export const newsData = [
     {
+        date: "2020/02/22",
+        title: "【新感覚】お出かけ/旅行の計画アプリ「Sassy （サッシー）」のiOS版が待望のリリース！",
+        category: "プレスリリース",
+        url: "https://prtimes.jp/main/html/rd/p/000000001.000051474.html",
+        target: "all"
+    },
+    {
+        date: "2020/03/30",
+        title: "【#エアお出かけ】Sassyユーザーの「行きたい場所ランキングin東京」発表（2020年3月）",
+        category: "プレスリリース",
+        url: "https://prtimes.jp/main/html/rd/p/000000002.000051474.html",
+        target: "all"
+    },
+    {
+        date: "2020/04/25",
+        title: "【#エアお出かけ】新感覚のお出かけ/旅行計画アプリ「Sassy （サッシー）」のAndroid版がリリース！（iOS版リニューアルも同時実施！）",
+        category: "プレスリリース",
+        url: "https://prtimes.jp/main/html/rd/p/000000004.000051474.html",
+        target: "all"
+    },
+    {
+        date: "2020/05/31",
+        title: "【テイクアウトの新しい形】飲食店応援プロジェクト「テイクアウトSassy」の事前登録スタート！１分で飲食店登録完了＆完全無料！",
+        category: "プレスリリース",
+        url: "https://prtimes.jp/main/html/rd/p/000000007.000051474.html",
+        target: "all"
+    },
+    {
+        date: "2020/06",
+        title: "株式会社NTTドコモ・ベンチャーズ主催の「STARTUP STUDIO（ベンチャー支援）」に採択されました",
+        category: "トピック",
+        url: "https://www.nttdocomo-v.com/",
+        target: "company"
+    },
+    {
+        date: "2020/06",
+        title: "株式会社ハンズオン主催の「HandsOn（スタートアップ支援）」に採択されました",
+        category: "トピック",
+        url: "https://prtimes.jp/main/html/rd/p/000000001.000054887.html",
+        target: "company"
+    },
+    {
+        date: "2020/06",
+        title: "東京都主催の「ASACスタートアップアクセラレーションプログラム」に採択されました",
+        category: "トピック",
+        url: "https://acceleration.tokyo.jp/detail.php?keyno=329",
+        target: "company"
+    },
+    {
+        date: "2020/06/30",
+        title: "【マイクロツーリズムの新定番】１分でお出かけプランが完成！お出かけ・旅行計画アプリ「Sassy」のプラン機能がリニューアル",
+        category: "プレスリリース",
+        url: "https://prtimes.jp/main/html/rd/p/000000008.000051474.html",
+        target: "all"
+    },
+    {
+        date: "2020/07/26",
+        title: "J-WAVEラジオ「CHINTAI COLORS OF WONDER」の『新しい旅を見つけよう！』のテーマでSassyが紹介されました",
+        category: "トピック",
+        url: "https://www.j-wave.co.jp/original/colorswonder/entry/200719.html",
+        target: "all"
+    },
+    {
+        date: "2020/08",
+        title: "日経XTechにて、NTTドコモベンチャーズ「/HuB」の記事で当社が紹介されました",
+        category: "トピック",
+        url: "https://special.nikkeibp.co.jp/atclh/NXT/20/nttdocomov0729_01/?fbclid=IwAR1fjYEGxlXcDBUej69PyjRTw5Xfg5tAKimPUxL2bt7eCYPsFrKkFMDYBNM",
+        target: "company"
+    },
+    {
+        date: "2020/09",
+        title: "東京都主催の「TCIC Pitch Campus2020アクセラレーションプログラム」に採択されました",
+        category: "トピック",
+        url: "https://www.tcic.jp/news/%e3%80%90%e5%8f%97%e4%bb%98%e9%96%8b%e5%a7%8b%e3%80%9107-31%e5%bf%9c%e5%8b%9f%e7%b7%a0%e3%82%81%e5%88%87%e3%82%8a-%e3%82%b3%e3%83%b3%e3%83%86%e3%83%b3%e3%83%84%e3%81%ae%e3%83%94%e3%83%83%e3%83%81/",
+        target: "company"
+    },
+    {
+        date: "2020/09/17",
+        title: "J-WAVEラジオ「TOKYO MORNING RADIO（ナビゲーター：別所哲也さん）」の『MORNING INSIGHT』でSassyが紹介されました",
+        category: "トピック",
+        url: "https://news.j-wave.co.jp/2020/09/post-6733.html",
+        target: "all"
+    },
+    {
+        date: "2020/09/30",
+        title: "【東京マイクロツーリズム】直感型おでかけアプリSassyに、友だち共有機能が追加！",
+        category: "プレスリリース",
+        url: "https://prtimes.jp/main/html/rd/p/000000010.000051474.html",
+        target: "all"
+    },
+    {
+        date: "2020/10/20",
+        title: "【これが観光の未来形】おでかけアプリSassyが大リニューアル！おでかけDJセレクト機能を新リリース！",
+        category: "プレスリリース",
+        url: "https://prtimes.jp/main/html/rd/p/000000011.000051474.html",
+        target: "all"
+    },
+    {
+        date: "2020/11/08",
+        title: "【20代に人気】直感型おでかけアプリ「Sassy」が、マイクロツーリズムWebメディア「Sassy-Trip」をスタート！",
+        category: "プレスリリース",
+        url: "https://prtimes.jp/main/html/rd/p/000000012.000051474.html",
+        target: "all"
+    },
+    {
+        date: "2020/12/01",
+        title: "当社と代表の西村が「STARTUP TIMES」の取材を受けました",
+        category: "トピック",
+        url: "https://startuptimes.jp/2020/12/01/213270/",
+        target: "company"
+    },
+    {
+        date: "2020/12/01",
+        title: "「X-Tech Innovation2020」の九州地区最終選考会に出場し、協賛企業賞を獲得しました",
+        category: "トピック",
+        url: "https://www.ibank.co.jp/xtech2020/",
+        target: "company"
+    },
+    {
+        date: "2020/12/01",
+        title: "沖縄を代表する8社が主催する「OKINAWA Startup Program2020-2021」に採択されました",
+        category: "トピック",
+        url: "https://www.okinawatimes.co.jp/articles/-/671543",
+        target: "company"
+    },
+    {
+        date: "2020/12/17",
+        title: "【より便利に】直感型おでかけアプリ『Sassy』新機能のお知らせ & 『鬼滅の刃』聖地巡礼スポット特集が大好評！",
+        category: "プレスリリース",
+        url: "https://prtimes.jp/main/html/rd/p/000000013.000051474.html",
+        target: "all"
+    },
+    {
+        date: "2021/03/16",
+        title: "「Okinawa Startup Program」のデモデイが開催され、代表の西村が発表いたしました",
+        category: "トピック",
+        url: "https://thebridge.jp/2021/03/okinawa-startup-program-2020-2021-demoday",
+        target: "all"
+    },
+    {
+        date: "2021/05/31",
+        title: "【観光/おでかけ】スポット直感型アプリ『Sassy』が、山形県と連携！地域コンテンツの作成、アプリ配信を開始。",
+        category: "プレスリリース",
+        url: "https://prtimes.jp/main/html/rd/p/000000014.000051474.html",
+        target: "all"
+    },
+    {
+        date: "2021/06/30",
+        title: "【観光/おでかけ】スポット直感型アプリ『Sassy』が青森県、岩手県および久慈市と連携！地域コンテンツの作成・配信を開始",
+        category: "プレスリリース",
+        url: "https://prtimes.jp/main/html/rd/p/000000015.000051474.html",
+        target: "all"
+    },
+    {
+        date: "2021/07/20",
+        title: "読売新聞朝刊（全国紙）にて、近場で休日を楽しみたいという要望に応えるサービスとしてSassyが紹介されました。",
+        category: "トピック",
+        url: "https://otekomachi.yomiuri.co.jp/lifestyle/20210721-OYTET50000/",
+        target: "all"
+    },
+    {
+        date: "2021/07/29",
+        title: "テレビ東京「田村淳が池袋 Innovation Council」に当社代表の西村が出演",
+        category: "プレスリリース",
+        url: "https://www.youtube.com/watch?v=uF1V4EnuBGs",
+        target: "company"
+    }, 
+    {
         date: "2021/07/31",
-        title: "1つ目【自治体との取組は３ヶ月連続！】スポット直感型アプリ『Sassy』が、三重県、富山県、山梨県、長野県、岐阜県および群馬県における観光コンテンツの作成・配信を開始",
+        title: "【自治体との取組は３ヶ月連続！】スポット直感型アプリ『Sassy』が、三重県、富山県、山梨県、長野県、岐阜県および群馬県における観光コンテンツの作成・配信を開始",
         category: "プレスリリース",
         url: "https://prtimes.jp/main/html/rd/p/000000017.000051474.html",
         target: "all"
     },
-    {
-        date: "2021/06/30",
-        title: "2つ目読売新聞朝刊（全国紙）にて、近場で休日を楽しみたいという要望に応えるサービスとしてSassyが紹介されました",
-        category: "トピック",
-        url: "https://otekomachi.yomiuri.co.jp/lifestyle/20210721-OYTET50000/",
-        target: "all"
-    },
-    {
-        date: "2021/06/30",
-        title: "3つ目読売新聞朝刊（全国紙）にて、近場で休日を楽しみたいという要望に応えるサービスとしてSassyが紹介されました",
-        category: "トピック",
-        url: "https://otekomachi.yomiuri.co.jp/lifestyle/20210721-OYTET50000/",
-        target: "all"
-    },
-    {
-        date: "2021/06/30",
-        title: "4つ目テスト",
-        category: "トピック",
-        url: "https://otekomachi.yomiuri.co.jp/lifestyle/20210721-OYTET50000/",
-        target: "all"
-    },
-    {
-        date: "2021/06/30",
-        title: "5つ目読売新聞朝刊（全国紙）にて、近場で休日を楽しみたいという要望に応えるサービスとしてSassyが紹介されました",
-        category: "プレスリリース",
-        url: "https://otekomachi.yomiuri.co.jp/lifestyle/20210721-OYTET50000/",
-        target: "all"
-    },
-    {
-        date: "2021/06/30",
-        title: "6つ目読売新聞朝刊（全国紙）にて、近場で休日を楽しみたいという要望に応えるサービスとしてSassyが紹介されました読売新聞朝刊（全国紙）にて、近場で休日を楽しみたいという要望に応えるサービスとしてSassyが紹介されました",
-        category: "トピック",
-        url: "https://otekomachi.yomiuri.co.jp/lifestyle/20210721-OYTET50000/",
-        target: "sassy"
-    }
 ]
