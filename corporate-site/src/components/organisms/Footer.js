f6c26fe3475546d440c3c727b3bd064000babb6a
nacky00 <nacky00eule@gmail.com>
2021-09-02 21:12:16 +0900

    update icons

--- corporate-site/src/components/organisms/Footer.js
+++ corporate-site/src/components/organisms/Footer.js
@@ -4,6 +4,45 @@ import { Link } from 'components/Router'
 import styled from 'styled-components'
 import Theme from 'theme'
 
+
+
+class Footer extends React.Component {
+    render() {
+            return(
+                <Wrapper>
+                    <Container>
+                        <Title>RelyonTrip</Title>
+                        <Contents>
+                            <Block>
+                                <Element><Link to="/sassy">おでかけアプリ「Sassy」</Link></Element>
+                                <Element><Link to="/">人生No.1飲食店SNS「Caviia」</Link></Element>
+                                <Element><Link to="/link">関連リンク集</Link></Element>
+                            </Block>
+                            <Block>
+                                <Element><Link to="/">RelyonTrip</Link></Element>
+                                <Element><a href="https://docs.google.com/forms/d/e/1FAIpQLSf0hMiTOxYSTDbsYl3uevU_f9uhFxnL4-9d2uXbQPAmSNo8zQ/viewform" target="_blank">お問い合わせ</a></Element>
+                                <Element><Link to="/privacy-policy">プライバシーポリシー</Link></Element>
+                                <Element><Link to="/terms">利用規約</Link></Element>
+                            </Block>
+                        </Contents>
+                        <Bottom>
+                            <div>
+                                <p>©RelyonTrip</p>
+                            </div>
+                            <div>
+                                <Icon href="https://www.instagram.com/sassy.trip/" target="_blank"><img src="/img/icon-instagram.png"/></Icon>
+                                <Icon href="https://twitter.com/akihito_sassy" target="_blank"><img src="/img/icon-twitter.png"/></Icon>
+                                <Icon href="https://www.facebook.com/akihito.nishimura.9" target="_blank"><img src="/img/icon-facebook.png"/></Icon>
+                            </div>
+                        </Bottom>
+                    </Container>
+                </Wrapper>
+            );
+    }
+}
+
+export default Footer
+
 const Wrapper = styled.div`
     background-color: ${Theme.color.black};
     padding: 1.6rem;
@@ -44,50 +83,15 @@ const Bottom = styled.div`
     display: flex;
     justify-content: space-between;
     p {
-        font-size: ${Theme.font.small};
+        font-size: ${Theme.font.medium};
         color: ${Theme.color.white};
-    }
-    a {
-        font-size: ${Theme.font.small};
-        font-weight: ${Theme.weight.normal};
-        color: ${Theme.color.white};
-        margin-left: 2rem;
     }
 `
 
-class Footer extends React.Component {
-    render() {
-            return(
-                <Wrapper>
-                    <Container>
-                        <Title>RelyonTrip</Title>
-                        <Contents>
-                            <Block>
-                                <Element><Link to="/sassy">おでかけアプリ「Sassy」</Link></Element>
-                                <Element><Link to="/">人生No.1飲食店SNS「Caviia」</Link></Element>
-                                <Element><Link to="/link">関連リンク集</Link></Element>
-                            </Block>
-                            <Block>
-                                <Element><Link to="/">RelyonTrip</Link></Element>
-                                <Element><a href="https://docs.google.com/forms/d/e/1FAIpQLSf0hMiTOxYSTDbsYl3uevU_f9uhFxnL4-9d2uXbQPAmSNo8zQ/viewform" target="_blank">お問い合わせ</a></Element>
-                                <Element><Link to="/privacy-policy">プライバシーポリシー</Link></Element>
-                                <Element><Link to="/terms">利用規約</Link></Element>
-                            </Block>
-                        </Contents>
-                        <Bottom>
-                            <div>
-                                <p>©RelyonTrip</p>
-                            </div>
-                            <div>
-                                <a href="https://www.instagram.com/sassy.trip/" target="_blank">IG</a>
-                                <a href="https://twitter.com/akihito_sassy" target="_blank">TW</a>
-                                <a href="https://www.facebook.com/akihito.nishimura.9" target="_blank">FB</a>
-                            </div>
-                        </Bottom>
-                    </Container>
-                </Wrapper>
-            );
+const Icon = styled.a`
+    margin-left: ${Theme.space.tiny};
+    img {
+        width: 32px;
+        height: 32px;
     }
-}
-
-export default Footer
+`
