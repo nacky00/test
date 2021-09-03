bc6923522c73a78c8e485b20ec59a71a89b63458
nacky00 <nacky00eule@gmail.com>
2021-09-03 11:04:34 +0900

    update thumbnail style

--- corporate-site/src/components/organisms/ProductSection.js
+++ corporate-site/src/components/organisms/ProductSection.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Theme from 'theme'
+import { Link } from 'components/Router'
 import { AppleButton } from 'components/atoms/AppleButton'
 import { GooglePlayButton } from 'components/atoms/GooglePlayButton'
 import { NormalWrapperStyle } from 'components/atoms/Wrapper/style'
@@ -14,14 +15,14 @@ export const ProductSection = props => {
             <p>観光・飲食系アプリ『Sassy (サッシー)』と『Caviia (キャビア)』を開発しています。</p>
             <Container>
                 <Item>
-                    <img src="/img/thumbnail-caviia.png" />
+                    <Image><img src="/img/thumbnail-caviia.png" /><Link to="https://caviia.com" /></Image>
                     <StoreButtonsContainer>
                         <AppleButton href={`https://apps.apple.com/us/app/caviia/id1546375547`}/>
                         <GooglePlayButton href={`https://play.google.com/store`}/>
                     </StoreButtonsContainer>
                 </Item>
                 <Item>
-                    <img src="/img/thumbnail-sassy.png" />
+                <Image><img src="/img/thumbnail-sassy.png" /><Link to="/sassy" /></Image>
                     <StoreButtonsContainer>
                         <AppleButton href={`https://apps.apple.com/jp/app/id1496112424`}/>
                         <GooglePlayButton href={`https://play.google.com/store/apps/details?id=com.relyontrip.sassy&referrer=utm_source%3Dsassy-lp%26anid%3Dadmob`}/>
@@ -48,12 +49,12 @@ const Container = styled.div`
     align-items: center;
     ${Theme.mediaQuery.mobile} {
         flex-direction: column;
-        div:first-child {
-            margin-bottom: ${Theme.space.tiny};
+        >div:first-child {
+            padding-bottom: ${Theme.space.tiny};
         }
     }
     ${Theme.mediaQuery.desktop} {
-        div:first-child {
+        >div:first-child {
             margin-right: ${Theme.space.medium};
         }
     }
@@ -69,4 +70,20 @@ const Item = styled.div`
     flex-direction: column;
     align-items: center;
     width: 50%;
+`
+
+const Image = styled.div`
+    position: relative;
+    width: 100%;
+    img {
+        width: 100%;
+    }
+    a {
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
 `
