6658336021c738ec4f705a41010f10ddf7d8009a
nacky00 <nacky00eule@gmail.com>
2021-09-03 15:56:25 +0900

    update contact section

--- corporate-site/src/components/organisms/ContactSection.js
+++ corporate-site/src/components/organisms/ContactSection.js
@@ -2,32 +2,33 @@ import React from 'react'
 import styled from 'styled-components'
 import Theme from 'theme'
 import { NormalWrapperStyle } from 'components/atoms/Wrapper/style'
+import { InnerSectionStyle } from 'components/atoms/Section/style'
+import { SectionHeader } from 'components/atoms/Text/style'
 
 export const ContactSection = props => {
     return (
         <Section>
             <Wrapper>
-                    <Title>Contact</Title>
-                    <Button><a href="https://docs.google.com/forms/d/e/1FAIpQLSf0hMiTOxYSTDbsYl3uevU_f9uhFxnL4-9d2uXbQPAmSNo8zQ/viewform" target="_blank">お問い合わせはこちら</a></Button>
+                <Header>Contact</Header>
+                <Button><a href="https://docs.google.com/forms/d/e/1FAIpQLSf0hMiTOxYSTDbsYl3uevU_f9uhFxnL4-9d2uXbQPAmSNo8zQ/viewform" target="_blank">お問い合わせはこちら</a></Button>
             </Wrapper>
         </Section>
     )
 }
 
 const Section = styled.section`
+    ${ InnerSectionStyle };
     background-color: ${Theme.color.goldDark};
-    padding: 3rem 0;
 `
 
 const Wrapper = styled.div`
     ${NormalWrapperStyle};
 `
 
-const Title = styled.div`
-    font-size: ${Theme.font.xlarge};
-    font-weight: ${Theme.weight.bold};
+const Header = styled.h1`
+    ${SectionHeader};
     color: ${Theme.color.grey400};
-    margin-bottom: 2rem;
+    margin-bottom: ${Theme.space.tiny};
 `
 
 const Button = styled.button`
@@ -36,10 +37,13 @@ const Button = styled.button`
     border-radius: 8px;
     a {
         display: block;
-        padding: 2rem 6rem;
+        padding: ${Theme.space.tiny} ${Theme.space.medium};
         color: ${Theme.color.white};
         font-size: ${Theme.font.large};
         font-weight: ${Theme.weight.normal};
+        ${Theme.mediaQuery.mobile} {
+            padding: ${Theme.space.tiny} ${Theme.space.tiny};
+        }
     }
     &:hover {
         background-color: rgba(0, 0, 0, 0.2);
